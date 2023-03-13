2b36f4237c7823ab2985dfce39429b9b0c5fd03f
Nguyễn Trọng Long <nguyentronglong@192.168.1.24>
2023-03-13 08:39:25 +0700

    Update_commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
@@ -6,9 +6,9 @@ import App from './App'
 import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  
 )
